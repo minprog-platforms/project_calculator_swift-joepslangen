655fe571e0cc56032e076dfb9b2e81004f22370e
joepslangen <joepslangen@hotmail.com>
2021-11-26 11:57:37 +0100

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Joep Slangen on 26/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
